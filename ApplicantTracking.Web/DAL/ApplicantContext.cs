6afdbeb2b9ecd299693048fe074bb18aba5932ea
Felipe Romero <feliperomero3@outlook.com>
2017-08-08 18:34:07 -0600

    Add empty ApplicantContext and Initializer classes.

---
+++ ApplicantTracking.Web/DAL/ApplicantContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApplicantTracking.Web.DAL
+{
+    public class ApplicantContext
+    {
+    }
+}
