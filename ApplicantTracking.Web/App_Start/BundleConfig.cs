11d142e7e9ee9b5e2f1cd16066e76443bfe592a0
Felipe Romero <feliperomero3@outlook.com>
2018-01-31 12:41:28 -0700

    Aplicar bootstrap-theme.

--- ApplicantTracking.Web/App_Start/BundleConfig.cs
+++ ApplicantTracking.Web/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace ApplicantTracking.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-theme.css",
                       "~/Content/site.css"));
         }
     }
