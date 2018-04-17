092e42eb6fd091bd527c89938bac9dab8479bc8b
Felipe Romero <feliperomero3@outlook.com>
2018-04-17 16:37:56 -0600

    Agregar nueva interfaz IApplicantRepository

--- ApplicantTracking.Data/Repositories/IApplicantRepository.cs
+++ ApplicantTracking.Data/Repositories/IApplicantRepository.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ApplicantTracking.Domain.Models;
 
 namespace ApplicantTracking.Data.Repositores
 {
     interface IApplicantRepository
     {
+        IEnumerable<Applicant> GetApplicants();
+
+        Applicant GetApplicant(int applicantId);
+
+        void AddApplicant(Applicant newApplicant);
+
+        void UpdateApplicant(Applicant applicant);
+
+        void DeleteApplicant(int applicantId);
     }
 }
