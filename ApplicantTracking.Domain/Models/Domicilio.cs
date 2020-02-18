2b3f18a58f159ab95445c7ed73cf9eb1200b8d16
Felipe Romero <feliperomero3@outlook.com>
2020-02-17 22:49:53 -0600

    Agregar más propiedades a entidad Domicilio.

--- ApplicantTracking.Domain/Models/Domicilio.cs
+++ ApplicantTracking.Domain/Models/Domicilio.cs
@@ -8,5 +8,8 @@
         public string EntreCalles { get; set; }
         public string Colonia { get; set; }
         public string CodigoPostal { get; set; }
+        public string Municipio { get; set; }
+        public string Estado { get; set; }
+        public string Pais { get; set; }
     }
 }
