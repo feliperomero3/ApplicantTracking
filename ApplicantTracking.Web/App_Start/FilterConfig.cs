d84d947b6d1b5618b8a088fdde13f48b88ea3d96
Felipe Romero <feliperomero3@outlook.com>
2020-03-01 20:59:15 -0600

    Setup up SSL in the Project.

--- ApplicantTracking.Web/App_Start/FilterConfig.cs
+++ ApplicantTracking.Web/App_Start/FilterConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace ApplicantTracking.Web
 {
@@ -8,6 +7,7 @@ namespace ApplicantTracking.Web
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
